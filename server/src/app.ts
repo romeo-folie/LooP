d412b6d4f394cd1c7db18fad0cf71ff02431b7a4
Romeo Folie <romeofolie1@gmail.com>
2025-02-21 18:26:19 +0000

    added cors dependency to enable resource sharing between server and client

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,14 @@
 import express, { Application } from 'express';
 import httpLogger from './middleware/logger-middleware';
 import router from './routes';
+import cors from 'cors';
 
 const app: Application = express();
+const corsOptions = { origin: process.env.CLIENT_URL as string }
 
 app.use(express.json());
-app.use(httpLogger); // Log all HTTP requests
-
+app.use(httpLogger);
+app.use(cors(corsOptions));
 app.use('/api', router);
 
 export default app;
