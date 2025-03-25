3ce3deb43689099e6d8fe05505048bda2176752f
Romeo Folie <romeofolie1@gmail.com>
2025-03-25 19:34:07 +0000

    generated icons and included them in webmanifest

--- client/src/context/notification-provider.tsx
+++ client/src/context/notification-provider.tsx
@@ -1,4 +1,10 @@
-import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import browserStore from "@/lib/browser-storage";
 import { toast } from "@/hooks/use-toast";
 
@@ -8,20 +14,24 @@ export type Notification = {
 };
 
 interface NotificationContextType {
-  notifications: Notification[],
-  notificationLength: number,
+  notifications: Notification[];
+  notificationLength: number;
   clearNotifications: () => void;
 }
 
-const NotificationContext = createContext<NotificationContextType | undefined>(undefined);
+const NotificationContext = createContext<NotificationContextType | undefined>(
+  undefined
+);
 
 export const useNotifications = () => {
-  const context = useContext(NotificationContext)
+  const context = useContext(NotificationContext);
   if (!context) {
-    throw new Error("useNotifications must be used within a NotificationProvider");
+    throw new Error(
+      "useNotifications must be used within a NotificationProvider"
+    );
   }
   return context;
-}
+};
 
 export const NotificationProvider = ({ children }: { children: ReactNode }) => {
   const [notifications, setNotifications] = useState<Notification[]>([]);
@@ -33,8 +43,8 @@ export const NotificationProvider = ({ children }: { children: ReactNode }) => {
         const { type, payload } = event.data || {};
         if (type === "IN_APP_ALERT") {
           browserStore.set("notifications", payload);
-          setNotifications((prev) => [...prev, payload])
-          setNotificationLength((prev) => prev + 1)
+          setNotifications((prev) => [...prev, payload]);
+          setNotificationLength((prev) => prev + 1);
           toast({
             title: payload.title,
             description: payload.body.message,
@@ -45,22 +55,26 @@ export const NotificationProvider = ({ children }: { children: ReactNode }) => {
   }, []);
 
   useEffect(() => {
-    const localNotifications = browserStore.get("notifications")
+    const localNotifications = browserStore.get("notifications");
     setNotifications(localNotifications.reverse());
     setNotificationLength(localNotifications.length);
   }, []);
 
   function clearNotifications() {
-    browserStore.set("notifications", [])
-    setNotifications([])
-    setNotificationLength(0)
+    browserStore.set("notifications", []);
+    setNotifications([]);
+    setNotificationLength(0);
   }
 
   const value: NotificationContextType = {
     notifications,
     notificationLength,
     clearNotifications,
-  }
+  };
 
-  return <NotificationContext.Provider value={value}>{children}</NotificationContext.Provider>
-}
+  return (
+    <NotificationContext.Provider value={value}>
+      {children}
+    </NotificationContext.Provider>
+  );
+};
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,7 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { VitePWA } from 'vite-plugin-pwa'
-
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
@@ -13,33 +12,48 @@ export default defineConfig({
       filename: "sw.js",
       devOptions: {
         enabled: true,
-        type: 'module',
+        type: "module",
       },
       manifest: {
-        name: "DSA Reminder App",
-        short_name: "DSA Reminders",
+        name: "LööP",
+        short_name: "LööP",
         theme_color: "#000000",
         background_color: "#000000",
         display: "standalone",
         start_url: ".",
-        // icons: [
-        //   {
-        //     src: "icon-192.png",
-        //     sizes: "192x192",
-        //     type: "image/png"
-        //   },
-        //   {
-        //     src: "icon-512.png",
-        //     sizes: "512x512",
-        //     type: "image/png"
-        //   }
-        // ]
-      }
-    })
+        icons: [
+          {
+            src: "/favicon.ico",
+            sizes: "48x48",
+            type: "image/x-icon",
+          },
+          {
+            src: "/pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "/apple-touch-icon-180x180.png",
+            sizes: "180x180",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
